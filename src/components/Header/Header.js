10322f4e6b65d5223768157c7795e4e47a7258ea
David Pam <niratpam@gmail.com>
2021-03-24 15:14:25 +0100

    Header Component Added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,96 @@
+import { Typography, Grid, Paper, useMediaQuery } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Navigation from '../Navigation/Navigation';
+import TestimonialImageDesktiop from '../../assets/img/testimonial-desktop.png';
+import TestimonialImageMobile from '../../assets/img/testimonial-mobile.png';
+
+const useStyles = makeStyles(theme => ({
+  heroTextBox: {
+    paddingTop: theme.spacing(8),
+    paddingLeft: theme.spacing(4),
+    [theme.breakpoints.up('md')]: {
+      paddingTop: theme.spacing(16),
+      paddingLeft: theme.spacing(16),
+    },
+  },
+  heroTextBig: {
+    fontFamily: 'Inter',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: theme.spacing(4),
+    lineHeight: '40px',
+    letterSpacing: '-0.02em',
+    width: '90%',
+    [theme.breakpoints.up('md')]: {
+      // width: '90%',
+      // height: '180px',
+      fontSize: theme.spacing(6),
+      lineHeight: '60px',
+      letterSpacing: '-0.02em',
+    },
+  },
+  heroSubText: {
+    fontFamily: 'Inter',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: theme.spacing(2),
+    lineHeight: '31px',
+    letterSpacing: '0.02em',
+    color: '#494949',
+    marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(2),
+    [theme.breakpoints.up('md')]: {
+      marginTop: theme.spacing(2),
+      marginBottom: '0',
+    }
+  },
+  testimonialImage: {
+    height: '506px',
+    width: '477px',
+    marginLeft: theme.spacing(2),
+    // backgroundImage: `url(${TestimonialImageDesktiop})`,
+    [theme.breakpoints.up('md')]: {
+      width: '536px',
+      height: '568px',
+      marginTop: theme.spacing(4),
+      marginLeft: '0',
+    },
+  },
+}));
+
+function Header() {
+  const isDesktop = useMediaQuery('(min-width: 960px)');
+  const classes = useStyles();
+
+  const TestimonialImage = isDesktop ? (
+    <img src={TestimonialImageDesktiop} alt='Testimonial Desktop' />
+  ) : (
+    <img src={TestimonialImageMobile} alt='Testimonial Mobile' />
+  );
+
+  return (
+    <div>
+      <Navigation />
+      <Grid container justify='center'>
+        <Grid item xs={12} md={6} className={classes.heroTextBox}>
+          <Typography className={classes.heroTextBig}>
+            Amazing Experiences from Our Wonderful Customers
+          </Typography>
+          <Typography className={classes.heroSubText}>
+            Here is what customers and vendors are saying about us, you can share your
+            stories with us too.
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          {/* <Paper className={classes.testimonialImage} /> */}
+          <Paper className={classes.testimonialImage} elevation={0}>
+            {TestimonialImage}
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,101 @@
+import { AppBar, Toolbar, Container, Grid, Divider, IconButton, Button, useMediaQuery } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import logo from '../../assets/img/logo.svg';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    [theme.breakpoints.down('md')]: {
+      boxShadow: 'none',
+    }
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  title: {
+    marginTop: theme.spacing(4),
+    [theme.breakpoints.up('md')]: {
+      marginTop: theme.spacing(1),
+    }
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex'
+    }
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none'
+    }
+  }
+}));
+
+function Navigation() {
+  const classes = useStyles();
+  const isDesktop = useMediaQuery('(min-width: 960px)');
+
+  const navDivider = isDesktop ? <Divider /> : null;
+  return (
+    <AppBar position='static' color='transparent' className={classes.root}>
+      <Container>
+        <Toolbar>
+          <IconButton
+            edge='start'
+            color='inherit'
+            aria-label='logo'
+            className={classes.title}
+          >
+            <img src={logo} alt='Logo' />
+          </IconButton>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <Button color='inherit'>About Us</Button>
+            <Button color='inherit'>Stories</Button>
+            <Button color='inherit'>Contact</Button>
+            <Button color='inherit'>Log In</Button>
+            <Button color='primary' variant='contained'>
+              Sign Up
+            </Button>
+          </div>
+        </Toolbar>
+      </Container>
+      {navDivider}
+      <div className={classes.sectionDesktop}>
+        <Container>
+          <Toolbar>
+            <Container>
+              <Grid container direction='row' justify='space-evenly'>
+                <Grid item>
+                  <Button>Marketing</Button>
+                </Grid>
+                <Grid item>
+                  <Button>Wholesale Center</Button>
+                </Grid>
+                <Grid item>
+                  <Button>Seller Center</Button>
+                </Grid>
+                <Grid item>
+                  <Button>Services</Button>
+                </Grid>
+                <Grid item>
+                  <Button>Internships</Button>
+                </Grid>
+                <Grid item>
+                  <Button>Events</Button>
+                </Grid>
+              </Grid>
+            </Container>
+          </Toolbar>
+        </Container>
+      </div>
+    </AppBar>
+  );
+}
+
+export default Navigation;
